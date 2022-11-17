2178d252d563fd90d3ab476c96bcb83bb46b7a9b
by030 <by030822@naver.com>
2022-11-18 04:58:18 +0900

    bottom Navigation 수정

--- app/src/main/java/com/example/ezapp3/FavoritesActivity.java
+++ app/src/main/java/com/example/ezapp3/FavoritesActivity.java
@@ -1,14 +1,54 @@
 package com.example.ezapp3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class FavoritesActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.favorites_button:
+//                        Intent intent1 = new Intent(getApplicationContext(), com.example.ezapp3.FindChargingStationActivity.class);
+//                        startActivity(intent1);
+                        Toast.makeText(getApplicationContext(), "favorites",
+                                Toast.LENGTH_SHORT).show();
+                        return true;
+
+                    case R.id.home_button:
+                        Intent intent2 = new Intent(getApplicationContext(), com.example.ezapp3.MainActivity.class);
+                        startActivity(intent2);
+//                        Toast.makeText(getApplicationContext(), "홈 화면 입니다.",
+//                                Toast.LENGTH_SHORT).show();
+                        return true;
+
+                    case R.id.profile_button:
+                        Intent intent3 = new Intent(getApplicationContext(), com.example.ezapp3.ElectricVehicleTips.class);
+                        startActivity(intent3);
+//                        Toast.makeText(getApplicationContext(), "정보 화면 입니다.",
+//                                Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
