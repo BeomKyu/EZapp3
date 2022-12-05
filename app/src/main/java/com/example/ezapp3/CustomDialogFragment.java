6920fb91062cd612ceb50294251eb1aa7def494b
by030 <by030822@naver.com>
2022-12-05 12:15:39 +0900

    레이아웃 수정

--- app/src/main/java/com/example/ezapp3/CustomDialogFragment.java
+++ app/src/main/java/com/example/ezapp3/CustomDialogFragment.java
@@ -84,7 +84,17 @@ public class CustomDialogFragment extends DialogFragment {
         });
         tvTitle.setText(title);
         tvNumber.setText(informationSplit[0]);
-        tvTime.setText(informationSplit[1]);
+        switch(informationSplit[1]){
+            case "충전소타입 : 01" : tvTime.setText("충전소타입 :  DC 차데모");break;
+            case "충전소타입 : 02" : tvTime.setText("충전소타입 :  AC 완속");break;
+            case "충전소타입 : 03" : tvTime.setText("충전소타입 : DC 차데모 + AC3 상");break;
+            case "충전소타입 : 04" : tvTime.setText("충전소타입 : DC 콤보");break;
+            case "충전소타입 : 05" : tvTime.setText("충전소타입 : DC 차데모+DC 콤보");break;
+            case "충전소타입 : 06" : tvTime.setText("충전소타입 : DC 차데모+AC3 상+DC 콤보");break;
+            case "충전소타입 : 07" : tvTime.setText("충전소타입 : AC3 상");break;
+            
+        }
+//        tvTime.setText(informationSplit[1]);
         tvWhere.setText(informationSplit[2]);
         tvBnm.setText(informationSplit[6]);//기관명
         tvBusiNm.setText(informationSplit[7]);//운영기관명
